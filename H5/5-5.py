49dcfa55ebb3407996d9406a3e5b04bbd45353ac
Don Wilbrink <don.wilbrink@gmail.com>
2023-09-18 19:42:12 +0200

    5-5 aangepast

--- H5/5-5.py
+++ H5/5-5.py
@@ -1,4 +1,5 @@
 from random import random
 
-num=int(random()*100)+1
-print("Een random getal tussen 1 en 100 is:", num)
+for i in range(10):
+    num=int(random()*1000)+1
+    print(i+1, "Een random getal tussen 1 en 1000 is:", num)
